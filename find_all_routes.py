cceda18c2177b47f6cbf81277ec5be81d005bc14
AFAD85 <antondiepenhorst@gmail.com>
2023-01-11 15:27:46 +0100

    added comments, removed old work

--- find_all_routes.py
+++ find_all_routes.py
@@ -2,94 +2,44 @@
 
 def find_routes(graph, start, end, routes=None, max_visit=3):
 
+    # initializes the routes list if it isn't already made
     if routes is None:
         routes = []
-        
-
+    
+    # checks to see if the start is a node in the graph
     if not start in graph:
         return ValueError("Start town not in routefinder")
     
-    
     route = []
     
+    # calls the recursive function, which will find all the routes
     recursive_find(graph, start, end, route, routes, 0, max_visit)
     print(routes)
-    # for node in graph[start]:
-    #     if node not in routes:
-    #         new_routes = find_routes(graph, node, end, routes, max_visit)
-    #         for new_route in new_routes:
-    #             routes.append(new_route)
+
     return routes
     
 def recursive_find(graph, start, end, route, routes, visited, max_visit):
     
-
+    # initializes the route by copying the route
     new_route = route.copy()
     new_route.append(start)
     
+    # checks to see if the route is complete, if so adds the new route to the list of routes    
     if start == end and visited > 0:
         routes.append(new_route)
-        
+    
+    # checks to see if the max number of visits has been reached, if so ends the function call    
     if visited == max_visit:
         return
     
+    # keeps track of the number of nodes visited, and feed that number into the next recursive call
     current_visited = visited+1
 
-    
+    # loops through all the nodes connected to the start node, and calls the recursive function on each of them
     for i in range (len(graph[start])):
         recursive_find(graph, graph[start][i], end, new_route , routes, current_visited, max_visit)
     return routes
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # if path is None:
-    #     path = []
-        
-    # visitedNodes = len(path)
-    # if visitedNodes >= max_visit:
-        
-    #     if start == end:
-    #         visitedNodes+=1
-    #         return path
-        
-    #     visitedNodes+=1
-    #     path.append(start)
-    #     return path
-    
-    # # Recursive case
-    # visitedNodes+=1
-    # path.append(start) 
-    
-       
-    # return find_routes(graph, graph[start][0], end, path)
-
-    # if (start == end) and ( visitedNodes <= max_visit):
-    #     return path
-    
-    # if not start in graph:
-    #     return []
-    
-    # routes = []
-    
-    # for node in graph[start]:
-    #     if path.count(node) < max_visit:
-    #         new_routes = find_routes(graph, node, end, path, max_visit)
-    #         for new_route in new_routes:
-    #             routes.append(new_route)
-    # return routes
-
-
-
-
-# graph = {'A': ['B', 'D', 'E'], 'B': ['C', 'E'], 'C': ['D','E'], 'D': ['C'], 'E': ['B']}
 
 graph = {'A': ['B', 'D', 'E'],
          'B': ['C'],
@@ -100,9 +50,6 @@ graph = {'A': ['B', 'D', 'E'],
 
 routes = find_routes(graph, 'C', 'C')
 
-# if routes:
-#     for route in routes:
-#         print(f"{route}\n")
 
 
 
