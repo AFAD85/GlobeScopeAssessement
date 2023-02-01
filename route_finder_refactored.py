b07794d1bb58f7ae3b446e66439d139bae093b98
AFAD85 <antondiepenhorst@gmail.com>
2023-02-01 14:35:31 +0100

    arguement counter refactored

--- route_finder_refactored.py
+++ route_finder_refactored.py
@@ -4,48 +4,10 @@ import re
 class RouteFinder():
 
  
-    def __init__(self, name):
+    def __init__(self, name, graph):
         self.name = name
-        
-        # creates 2 dictionaries one to hold the possible edges (keys) with their corresonding distances (values), 
-        # the second holds all possible edges from every start node (keys) to every neighbouring node (values)
-        # Note: different use of edge and edges
-        self.edge_distances = dict()
-        self.edges = dict()
-        
-    def __str__(self):
-        # this function determines what happens when the object is printed
-        return f"Route Finder: {self.name}\n This route finder has the following edges : {self.edges}\n and the following edge distances : {self.edge_distances}"
+        self.graph = graph
 
-
-    def add_edge(self, edge):
-        
-        # checks if the edge input is in the correct format
-        if re.match(r'^[A-E]{2}[0-9]$', edge):
-            
-            # if check is passed : creates variables to store the edge name and distance
-            edge_distances_key = edge[:2]
-            edge_distances_value = int(edge[2])
-            
-            # checks if the start node of the edge is already known in the edges dictionary, if not adds it and the corresponding edge
-            if edge[0] not in self.edges:
-                self.edges[edge[0]] = [edge[1]]
-            
-            # if the start node is already known, adds the new edge
-            else:
-                self.edges[edge[0]].append(edge[1])
-            
-            # adds the input to the edge_distances dictionary
-            self.edge_distances[edge_distances_key] = edge_distances_value
-            
-        else:
-            print("Error: Invalid input, please enter in the form of two capital letters A-E followed by an integer 0-9")
-            
-
-    
-    def _get_edge_input_distance(self, edge):
-        
-        
         
     def get_route_distance(self, nodes):
         
@@ -56,8 +18,8 @@ class RouteFinder():
         
         # gets the distance of each pair of towns from the routes dictionary and adds these to the distance variable
         for town_pair in node_pairs:
-            if town_pair in self.edge_distances:
-                sub_distance = self.edge_distances.get(town_pair)
+            if town_pair in self.graph.edge_distances:
+                sub_distance = self.graph.edge_distances.get(town_pair)
                 distance += sub_distance
             else:
                 return "NO SUCH ROUTE"
@@ -75,37 +37,21 @@ class RouteFinder():
         max_distance = max_distance
         
         # User should define either max_stops, exact_stops or max_distance
-        if not (max_stops or exact_stops or max_distance):
-            return ValueError("Missing arguements, please define exactly one of max_stops, exact_stops or max_distance")
+        # if none are defined or too many are defined an error is raised
+        args_count = sum(1 for arg in [max_stops, exact_stops, max_distance] if arg is not None)
+        if args_count != 1:
+            return "Error : Please define exactly one of max_stops, exact_stops, or max_distance"
         
-        # Counter to check how many arguements have been defined
-        args_count = 0
-        
-        # Check if max_stops is defined
-        if max_stops != None:
-            args_count += 1
-        
-        # Check if exact_stops is defined
-        if exact_stops != None:
-            args_count += 1
-            
-        # Check if max_distance is defined
-        if max_distance != None:
-            args_count += 1
-
-        # If more than one arguement is defined, raises an error
-        if args_count > 1:
-            return ValueError("Too many arguements, please define exactly one of max_stops, exact_stops or max_distance")
 
         # If max_stops is defined, call the _find_routes function with the max_stops arguement
         if max_stops:
-            possible_routes = self._find_routes(self.edges, start, end, max_stops=max_stops)
+            possible_routes = self._find_routes(self.graph.edges, start, end, max_stops=max_stops)
             return possible_routes
         
         # If exact_stops is defined, call the _find_routes functions with the max_stops arguement, 
         # and after running removes the routes that do not meet the exact stops requirement
         if exact_stops:
-            possible_routes = self._find_routes(self.edges, start, end, max_stops=exact_stops)
+            possible_routes = self._find_routes(self.graph.edges, start, end, max_stops=exact_stops)
             
             # checks if the route lenght is equal to the exact_stops arguement, if not removes it from the list
             for route in possible_routes:
@@ -116,7 +62,7 @@ class RouteFinder():
         
         # If max_distance is defined, call the _find_routes function with the max_distance arguement
         if max_distance:
-            possible_routes = self._find_routes(self.edges, start, end, max_distance=max_distance)
+            possible_routes = self._find_routes(self.graph.edges, start, end, max_distance=max_distance)
             return possible_routes
         
         
@@ -202,7 +148,7 @@ class RouteFinder():
     def get_shortest_route(self, Town1, Town2):
         # first finds all routes between the two towns(nodes) with the max amount of stops set to the number of nodes in the graph
         # then finds the shortest route by comparing the distances of each route
-        all_routes = self.get_possible_routes(Town1, Town2, max_stops=len(self.edges))
+        all_routes = self.get_possible_routes(Town1, Town2, max_stops=len(self.graph.edges))
         route_distances = []
         
         # loops through all the routes and finds the distance of each route
